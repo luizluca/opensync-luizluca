1f8e9ce7e8f9b05ed19f3f72a1119fbf3691a096
Bjoern Ricks <bjoern.ricks@googlemail.com>
2008-12-21 18:31:55 +0000

    moved doxygen private group declaration in private header
    git-svn-id: http://svn.opensync.org/trunk@4424 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

--- opensync/group/opensync_group_internals.h
+++ opensync/group/opensync_group_internals.h
@@ -2,36 +2,34 @@
  * libopensync - A synchronization framework
  * Copyright (C) 2004-2006  Armin Bauer <armin.bauer@desscon.com>
  * Copyright (C) 2008       Daniel Gollub <dgollub@suse.de>
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- * 
+ *
  */
- 
+
 
 #ifndef _OPENSYNC_GROUP_INTERNALS_H_
 #define _OPENSYNC_GROUP_INTERNALS_H_
 
 /**
- * @defgroup OSyncGroupPrivate OpenSync Group Module Private API
- * @ingroup OSyncPrivate
  * @defgroup OSyncGroupInternalAPI OpenSync Group Internals
  * @ingroup OSyncGroupPrivate
  * @brief The private API of opensync
- * 
+ *
  * This gives you an insight in the private API of opensync.
- * 
+ *
  */
 /*@{*/
 
@@ -57,21 +55,21 @@ struct OSyncGroup {
 	osync_bool merger_enabled;
 	/** The configured converter status of this group */
 	osync_bool converter_enabled;
-	
+
 #ifdef OPENSYNC_UNITTESTS
 	char *schemadir;
-#endif /* OPENSYNC_UNITTESTS*/	
+#endif /* OPENSYNC_UNITTESTS*/
 	int ref_count;
 };
 
 #ifdef OPENSYNC_UNITTESTS
 /** @brief Set the schemadir for configuration validation to a custom directory.
- *  This is actually only inteded for UNITTESTS to run tests without 
+ *  This is actually only inteded for UNITTESTS to run tests without
  *  having OpenSync installed.
- * 
+ *
  * @param group Pointer to group
  * @param schemadir Custom schemadir path
- * 
+ *
  */
 OSYNC_TEST_EXPORT void osync_group_set_schemadir(OSyncGroup *group, const char *schemadir);
 #endif /* OPENSYNC_UNITTESTS*/
