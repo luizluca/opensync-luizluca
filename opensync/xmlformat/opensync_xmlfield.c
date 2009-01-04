luizluca/opensync-luizluca

commit 63153114582aa58217de21a7ad1d100bcf9177ac

Introduce following new public API functions: osync_xmlfield_get_child osync_xmlfield_get_parent osync_xmlfield_get_prev osync_xmlfield_get_value
Additionally the OSyncXMLField and OSyncXMLFormat supporting multiple
levels of XML nodes. The osync_xmlfield_{attribute,key}_ interfaces are
still available.

Supporting multiple fields was acutally a positive side-effect when
wrapping each node with a OSyncXMLField. This allows full iteration
across all nodes, completely wrapped by OSyncXMLField. This also allows
now to break-out the xmlformat-plugin from the core. Since now internal
interface are required anymore.



git-svn-id: http://svn.opensync.org/trunk@4933 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

opensync/xmlformat/opensync_xmlfield.c
@@ -27,10 +27,28 @@
 #include "opensync-xmlformat_internals.h"
 #include "opensync_xmlformat_private.h"		/* FIXME: direct access of private header */
 
+#include "opensync_xmlfield.h"
 #include "opensync_xmlfield_private.h"
 #include "opensync_xmlfield_internals.h"
 
-OSyncXMLField *osync_xmlfield_new_node(OSyncXMLFormat *xmlformat, xmlNodePtr node, OSyncError **error)
+OSyncXMLField *osync_xmlfield_new_node(xmlNodePtr node, OSyncError **error)
+{
+	OSyncXMLField *xmlfield = osync_try_malloc0(sizeof(OSyncXMLField), error);
+	if(!xmlfield) {
+		osync_trace(TRACE_ERROR, "%s: %s" , __func__, osync_error_print(error));
+		return NULL;
+	}
+	
+	xmlfield->node = node;
+	node->_private = xmlfield;
+	
+	// We don't know if the parsed xmlformat got sorted xmlfield -> unsorted
+	xmlfield->sorted = FALSE;
+	
+	return xmlfield;
+}
+
+OSyncXMLField *osync_xmlfield_new_xmlfield(OSyncXMLField *parent, xmlNodePtr node, OSyncError **error)
 {
 	OSyncXMLField *xmlfield = osync_try_malloc0(sizeof(OSyncXMLField), error);
 	if(!xmlfield) {
@@ -40,20 +58,64 @@ OSyncXMLField *osync_xmlfield_new_node(OSyncXMLFormat *xmlformat, xmlNodePtr nod
 	
 	xmlfield->next = NULL;
 	xmlfield->node = node;
-	xmlfield->prev = xmlformat->last_child;
+	xmlfield->parent = parent;
 	node->_private = xmlfield;
 	
-	if(!xmlformat->first_child)
-		xmlformat->first_child = xmlfield;
-	if(xmlformat->last_child)
-		xmlformat->last_child->next = xmlfield;
-	xmlformat->last_child = xmlfield;
-	xmlformat->child_count++;
+	if (parent) {
+		xmlfield->prev = parent->child;
+		if (xmlfield->prev)
+			xmlfield->prev->next = xmlfield;
+		parent->child = xmlfield;
+	}
+
 
 	// We don't know if the parsed xmlformat got sorted xmlfield -> unsorted
 	xmlfield->sorted = FALSE;
 	
 	return xmlfield;
+}
+
+osync_bool osync_xmlfield_parse(OSyncXMLField *parent, xmlNodePtr node, OSyncXMLField **first_child, OSyncXMLField **last_child, OSyncError **error)
+{
+	OSyncXMLField *xmlfield = NULL;
+
+	if (first_child)
+		*first_child = NULL;
+
+	while (node != NULL) {
+
+		xmlfield = osync_xmlfield_new_xmlfield(parent, node, error);
+		if (!xmlfield)
+			goto error;
+
+		if (first_child && !(*first_child)) {
+			*first_child = xmlfield;
+		}
+
+		if (node->children && node->children->type == XML_ELEMENT_NODE)
+			if (!osync_xmlfield_parse(xmlfield, node->children, NULL, NULL, error))
+				goto error_and_free;
+
+		node = node->next;
+	}
+
+	if (last_child)
+		*last_child = xmlfield;
+
+	return TRUE;
+
+error_and_free:
+	osync_xmlfield_free(xmlfield);
+error:
+
+	if (last_child)
+		*last_child = NULL;
+
+	if (first_child)
+		*first_child = NULL;
+
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s" , __func__, osync_error_print(error));
+	return FALSE;
 }
 
 void osync_xmlfield_free(OSyncXMLField *xmlfield)
@@ -150,7 +212,7 @@ OSyncXMLField *osync_xmlfield_new(OSyncXMLFormat *xmlformat, const char *name, O
 	
 	node = xmlNewTextChild(xmlDocGetRootElement(xmlformat->doc), NULL, BAD_CAST name, NULL);
 	
-	xmlfield = osync_xmlfield_new_node(xmlformat, node, error);
+	xmlfield = osync_xmlfield_new_xmlfield(xmlformat->last_child, node, error);
 	if(!xmlfield) {
 		xmlUnlinkNode(node);
 		xmlFreeNode(node);
@@ -185,9 +247,32 @@ void osync_xmlfield_set_name(OSyncXMLField *xmlfield, const char *name)
 
 OSyncXMLField *osync_xmlfield_get_next(OSyncXMLField *xmlfield)
 {
-	osync_assert(xmlfield);
-	
+	osync_return_val_if_fail(xmlfield, NULL);
 	return xmlfield->next;
+}
+
+OSyncXMLField *osync_xmlfield_get_prev(OSyncXMLField *xmlfield)
+{
+	osync_return_val_if_fail(xmlfield, NULL);
+	return xmlfield->prev;
+}
+
+OSyncXMLField *osync_xmlfield_get_parent(OSyncXMLField *xmlfield)
+{
+	osync_return_val_if_fail(xmlfield, NULL);
+	return xmlfield->parent;
+}
+
+OSyncXMLField *osync_xmlfield_get_child(OSyncXMLField *xmlfield)
+{
+	osync_return_val_if_fail(xmlfield, NULL);
+	return xmlfield->child;
+}
+
+const char *osync_xmlfield_get_value(OSyncXMLField *xmlfield)
+{
+	osync_return_val_if_fail(xmlfield, "");
+	return (const char *)osync_xml_node_get_content(xmlfield->node);
 }
 
 const char *osync_xmlfield_get_attr(OSyncXMLField *xmlfield, const char *attr)

opensync/xmlformat/opensync_xmlformat.c
@@ -72,44 +72,42 @@ OSyncXMLFormat *osync_xmlformat_new(const char *objtype, OSyncError **error)
 OSyncXMLFormat *osync_xmlformat_parse(const char *buffer, unsigned int size, OSyncError **error)
 {
 	OSyncXMLFormat *xmlformat = NULL;
+	OSyncXMLField *xmlfield = NULL;
 	xmlNodePtr cur = NULL;
 	osync_trace(TRACE_ENTRY, "%s(%p, %i, %p)", __func__, buffer, size, error);
 	osync_assert(buffer);
 
 	xmlformat = osync_try_malloc0(sizeof(OSyncXMLFormat), error);
-	if(!xmlformat) {
-		osync_trace(TRACE_EXIT_ERROR, "%s: %s" , __func__, osync_error_print(error));
-		return NULL;
-	}
+	if (!xmlformat)
+		goto error;
 	
 	xmlformat->doc = xmlReadMemory(buffer, size, NULL, NULL, XML_PARSE_NOBLANKS);
 	if(!xmlformat->doc) {
-		g_free(xmlformat);
+		osync_free(xmlformat);
 		osync_error_set(error, OSYNC_ERROR_GENERIC, "Could not parse XML.");
-		osync_trace(TRACE_EXIT_ERROR, "%s: %s" , __func__, osync_error_print(error));
-		return NULL;	
+		goto error;
 	}
 
 	xmlformat->ref_count = 1;
-	xmlformat->first_child = NULL;
-	xmlformat->last_child = NULL;
-	xmlformat->child_count = 0;
 	xmlformat->doc->_private = xmlformat;
-		
+
+	
 	cur = xmlDocGetRootElement(xmlformat->doc);
-	cur = cur->children;
-	while (cur != NULL) {
-		OSyncXMLField *xmlfield = osync_xmlfield_new_node(xmlformat, cur, error);
-		if(!xmlfield) {
-			osync_xmlformat_unref(xmlformat);
-			osync_trace(TRACE_EXIT_ERROR, "%s: %s" , __func__, osync_error_print(error));
-			return NULL;
-		}
-		cur = cur->next;
-	}
+	if (!(xmlfield = osync_xmlfield_new_node(cur, error)))
+		goto error;
+
+	if (!osync_xmlfield_parse(xmlfield, cur->children, &xmlformat->first_child, &xmlformat->last_child, error))
+		goto error;
+
+	xmlformat->xmlfield = xmlfield;
+	xmlformat->child_count = osync_xmlfield_get_key_count(xmlfield);
 
 	osync_trace(TRACE_EXIT, "%s: %p", __func__, xmlformat);
 	return xmlformat;
+
+error:
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s" , __func__, osync_error_print(error));
+	return NULL;
 }
 
 OSyncXMLFormat *osync_xmlformat_ref(OSyncXMLFormat *xmlformat)

opensync.sym
@@ -716,6 +716,7 @@ osync_version_unref
 osync_xmlfield_add_key_value
 osync_xmlfield_get_attr
 osync_xmlfield_get_attr_count
+osync_xmlfield_get_child
 osync_xmlfield_get_key_count
 osync_xmlfield_get_key_value
 osync_xmlfield_get_name
@@ -724,6 +725,9 @@ osync_xmlfield_get_nth_attr_name
 osync_xmlfield_get_nth_attr_value
 osync_xmlfield_get_nth_key_name
 osync_xmlfield_get_nth_key_value
+osync_xmlfield_get_parent
+osync_xmlfield_get_prev
+osync_xmlfield_get_value
 osync_xmlfield_new
 osync_xmlfield_set_attr
 osync_xmlfield_set_key_value

opensync/xmlformat/opensync_xmlfield.h
@@ -64,6 +64,34 @@ OSYNC_EXPORT void osync_xmlfield_set_name(OSyncXMLField *xmlfield, const char *n
 OSYNC_EXPORT OSyncXMLField *osync_xmlfield_get_next(OSyncXMLField *xmlfield);
 
 /**
+ * @brief Get the previous xmlfield
+ * @param xmlfield Pointer to the xmlfield object
+ * @return A pointer to the previous xmlfield or NULL if there is no more xmlfield
+ */
+OSYNC_EXPORT OSyncXMLField *osync_xmlfield_get_prev(OSyncXMLField *xmlfield);
+
+/**
+ * @brief Get the parent xmlfield
+ * @param xmlfield Pointer to the xmlfield object
+ * @return A pointer to the parent xmlfield or NULL if there is no parent xmlfield
+ */
+OSYNC_EXPORT OSyncXMLField *osync_xmlfield_get_parent(OSyncXMLField *xmlfield);
+
+/**
+ * @brief Get the child xmlfield
+ * @param xmlfield Pointer to the xmlfield object
+ * @return A pointer to the child xmlfield or NULL if there is no child xmlfield
+ */
+OSYNC_EXPORT OSyncXMLField *osync_xmlfield_get_child(OSyncXMLField *xmlfield);
+
+/**
+ * @brief Get the value of the xmlfield
+ * @param xmlfield Pointer to the xmlfield object
+ * @return String pointer to the value. If the content is empty, "" gets returned.
+ */
+OSYNC_EXPORT const char *osync_xmlfield_get_value(OSyncXMLField *xmlfield);
+
+/**
  * @brief Get the value of a attribute of an xmlfield
  * @param xmlfield Pointer to the xmlfield object
  * @param attr The name of the attribute

opensync/xmlformat/opensync_xmlfield_internals.h
@@ -32,14 +32,15 @@
 /*@{*/
 
 /**
- * @brief Creates a new xmlfield object which will be added to the end of xmlfields of the xmlformat object.
+ * @brief Creates a new xmlfield object from xmlNodePtr.
  *  The returned object will be freed with the xmlformat object.
- * @param xmlformat The pointer to a xmlformat object
  * @param node The node must be already inserted at the end of childs of the xmlDoc root element
  * @param error The error which will hold the info in case of an error
  * @return The pointer to the newly allocated xmlfield object or NULL in case of error
  */
-OSyncXMLField *osync_xmlfield_new_node(OSyncXMLFormat *xmlformat, xmlNodePtr node, OSyncError **error);
+OSyncXMLField *osync_xmlfield_new_node(xmlNodePtr node, OSyncError **error);
+
+osync_bool osync_xmlfield_parse(OSyncXMLField *parent, xmlNodePtr node, OSyncXMLField **first_child, OSyncXMLField **last_child, OSyncError **error);
 
 /**
  * @brief Frees a already unlinked xmlfield object

opensync/xmlformat/opensync_xmlfield_private.h
@@ -33,6 +33,10 @@
  * @brief Represent a XMLField object
  */
 struct OSyncXMLField {
+	/** The pointer to the parent xmlfield */
+	OSyncXMLField *parent;
+	/** The pointer to the first child xmlfield */
+	OSyncXMLField *child;
 	/** The pointer to the next xmlfield */
 	OSyncXMLField *next;
 	/** The pointer to the previous xmlfield */
@@ -41,6 +45,8 @@ struct OSyncXMLField {
 	xmlNodePtr node;
 	/** Sorted status */
 	osync_bool sorted;
+        /** Child count */
+        unsigned int child_count;
 };
 /*@}*/
 

opensync/xmlformat/opensync_xmlformat_private.h
@@ -36,6 +36,8 @@
 struct OSyncXMLFormat {
 	/** The reference counter for this object */
 	int ref_count;
+	/** The root xmlfield */
+	OSyncXMLField *xmlfield;
 	/** The first xmlfield */
 	OSyncXMLField *first_child;
 	/** The last xmlfield */

commit d2ebd209a00c564d7806180e131e9ff31d5d3da5

Fix child_count of XMLFormat for XMLFormat's which got assemble by osync_xmlfield_new() (all xmlformat-converter plugins!).
This regression was introduced by r4933



git-svn-id: http://svn.opensync.org/trunk@4988 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

opensync/xmlformat/opensync_xmlfield.c
@@ -75,6 +75,32 @@ OSyncXMLField *osync_xmlfield_new_xmlfield(OSyncXMLField *parent, xmlNodePtr nod
 	return xmlfield;
 }
 
+OSyncXMLField *osync_xmlfield_new_xmlformat(OSyncXMLFormat *xmlformat, xmlNodePtr node, OSyncError **error)
+{
+	OSyncXMLField *xmlfield = osync_try_malloc0(sizeof(OSyncXMLField), error);
+	if(!xmlfield) {
+		osync_trace(TRACE_ERROR, "%s: %s" , __func__, osync_error_print(error));
+		return NULL;
+	}
+	
+	xmlfield->next = NULL;
+	xmlfield->node = node;
+	xmlfield->prev = xmlformat->last_child;
+	node->_private = xmlfield;
+	
+	if(!xmlformat->first_child)
+		xmlformat->first_child = xmlfield;
+	if(xmlformat->last_child)
+		xmlformat->last_child->next = xmlfield;
+	xmlformat->last_child = xmlfield;
+	xmlformat->child_count++;
+
+	// We don't know if the parsed xmlformat got sorted xmlfield -> unsorted
+	xmlfield->sorted = FALSE;
+	
+	return xmlfield;
+}
+
 osync_bool osync_xmlfield_parse(OSyncXMLField *parent, xmlNodePtr node, OSyncXMLField **first_child, OSyncXMLField **last_child, OSyncError **error)
 {
 	OSyncXMLField *xmlfield = NULL;
@@ -212,7 +238,7 @@ OSyncXMLField *osync_xmlfield_new(OSyncXMLFormat *xmlformat, const char *name, O
 	
 	node = xmlNewTextChild(xmlDocGetRootElement(xmlformat->doc), NULL, BAD_CAST name, NULL);
 	
-	xmlfield = osync_xmlfield_new_xmlfield(xmlformat->last_child, node, error);
+	xmlfield = osync_xmlfield_new_xmlformat(xmlformat, node, error);
 	if(!xmlfield) {
 		xmlUnlinkNode(node);
 		xmlFreeNode(node);

opensync/xmlformat/opensync_xmlformat.c
@@ -51,10 +51,8 @@ OSyncXMLFormat *osync_xmlformat_new(const char *objtype, OSyncError **error)
 	osync_assert(objtype);
 	
 	xmlformat = osync_try_malloc0(sizeof(OSyncXMLFormat), error);
-	if(!xmlformat) {
-		osync_trace(TRACE_EXIT_ERROR, "%s: %s" , __func__, osync_error_print(error));
-		return NULL;
-	}
+	if (!xmlformat)
+		goto error;
 
 	xmlformat->doc = xmlNewDoc(BAD_CAST "1.0");
 	xmlformat->doc->children = xmlNewDocNode(xmlformat->doc, NULL, BAD_CAST objtype, NULL);
@@ -64,9 +62,16 @@ OSyncXMLFormat *osync_xmlformat_new(const char *objtype, OSyncError **error)
 	xmlformat->child_count = 0;
 	xmlformat->sorted = FALSE;
 	xmlformat->doc->_private = xmlformat;
+
+	if (!(xmlformat->xmlfield = osync_xmlfield_new_node(xmlformat->doc->children, error)))
+		goto error;
 	
 	osync_trace(TRACE_EXIT, "%s: %p", __func__, xmlformat);
 	return xmlformat;
+
+error:
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s" , __func__, osync_error_print(error));
+	return NULL;
 }
 
 OSyncXMLFormat *osync_xmlformat_parse(const char *buffer, unsigned int size, OSyncError **error)
@@ -100,6 +105,9 @@ OSyncXMLFormat *osync_xmlformat_parse(const char *buffer, unsigned int size, OSy
 		goto error;
 
 	xmlformat->xmlfield = xmlfield;
+	/* TODO: get key-count value with a sepcial parase function which
+	 * counts the first-level, by incrementing the node->doc->_private pointer
+	 */
 	xmlformat->child_count = osync_xmlfield_get_key_count(xmlfield);
 
 	osync_trace(TRACE_EXIT, "%s: %p", __func__, xmlformat);