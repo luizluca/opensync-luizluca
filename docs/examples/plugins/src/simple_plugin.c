08049c28709f24db1a8f7f3e18ea0873f961e364
Daniel Gollub <gollub@b1-systems.de>
2009-03-28 19:15:46 +0000

    Dropped legacy includes.
    git-svn-id: http://svn.opensync.org/trunk@5401 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

--- docs/examples/plugins/src/simple_plugin.c
+++ docs/examples/plugins/src/simple_plugin.c
@@ -2,10 +2,8 @@
 #include <opensync/opensync-data.h>
 #include <opensync/opensync-format.h>
 #include <opensync/opensync-plugin.h>
-#include <opensync/opensync-context.h>
 #include <opensync/opensync-helper.h>
 #include <opensync/opensync-version.h>
-#include <opensync/opensync_list.h>
 
 #include <string.h>
 
