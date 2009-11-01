luizluca/opensync-luizluca

commit a2b5d8a87779107d9e8bcb4e12daec36da762703

Plugins of type OSYNC_START_TYPE_EXTERNAL needs to be able to spawn an external command to run the actual plugin code.
This patch allows a member to set an ExternalCommand in the plugin configuration file.
This command will be executed by the proxy to start up the external process.

The external_command should be a string in printf format, with one %s.
Before the command is executed, a variant of printf will be called
to replace the %s with the path to the plugin pipe.

Example:
<ExternalPlugin>
  <ExternalCommand>thunderbird -mozilla-sync %s</ExternalCommand>
</ExternalPlugin>

Added to public API:
osync_plugin_config_get_externalplugin
osync_plugin_config_set_externalplugin
osync_plugin_externalplugin_new
osync_plugin_externalplugin_unref
osync_plugin_externalplugin_ref
osync_plugin_externalplugin_get_external_command
osync_plugin_externalplugin_set_external_command
(as well as the changed format for the plugin configuration file)

Changed internal interface:
From
  osync_bool osync_client_proxy_spawn(OSyncClientProxy *proxy, OSyncStartType type, const char *path, OSyncError **error)
To
  osync_bool osync_client_proxy_spawn(OSyncClientProxy *proxy, OSyncStartType type, const char *path, const char* external_command, OSyncError **error)




git-svn-id: http://svn.opensync.org/trunk@5896 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

opensync/group/opensync_member.c
@@ -832,6 +832,18 @@ OSyncList *osync_member_get_objtypes(OSyncMember *member) {
 	return new_list;
 }
 
+const char *osync_member_get_external_command(OSyncMember *member)
+{
+	osync_assert(member);
+	OSyncError *error;
+	OSyncPluginConfig *config = osync_member_get_config_or_default(member, &error);
+	if (config) {
+		OSyncPluginExternalPlugin *externalplugin = osync_plugin_config_get_externalplugin(config);
+		if (externalplugin) return osync_plugin_externalplugin_get_external_command(externalplugin);
+	}
+	return NULL;
+}
+
 osync_bool osync_member_objtype_enabled(OSyncMember *member, const char *objtype)
 {
 	OSyncObjTypeSink *sink = NULL;

misc/schemas/plugin_config.xsd
@@ -9,6 +9,7 @@
         <xsd:element maxOccurs="1" minOccurs="0" name="Connection" type="Connection"/>
         <xsd:element maxOccurs="1" minOccurs="0" name="Localization" type="Localization"/>
         <xsd:element maxOccurs="1" minOccurs="0" name="Resources" type="Resources"/>
+        <xsd:element maxOccurs="1" minOccurs="0" name="ExternalPlugin" type="ExternalPlugin"/>
       </xsd:sequence>
       <xsd:attribute name="version" type="xsd:string"/>
     </xsd:complexType>
@@ -161,6 +162,12 @@
       <xsd:element maxOccurs="1" minOccurs="0" name="Network" type="ConnectionNetwork" />
       <xsd:element maxOccurs="1" minOccurs="0" name="Serial" type="ConnectionSerial" />
       <xsd:element maxOccurs="1" minOccurs="0" name="USB" type="ConnectionUSB" />
+    </xsd:all>
+  </xsd:complexType>
+
+  <xsd:complexType name="ExternalPlugin">
+    <xsd:all>
+      <xsd:element maxOccurs="1" minOccurs="1" name="ExternalCommand" type="xsd:string" />
     </xsd:all>
   </xsd:complexType>
 

opensync.sym
@@ -511,6 +511,7 @@ osync_plugin_config_get_advancedoption_value_by_name
 osync_plugin_config_get_advancedoptions
 osync_plugin_config_get_authentication
 osync_plugin_config_get_connection
+osync_plugin_config_get_externalplugin
 osync_plugin_config_get_localization
 osync_plugin_config_get_resources
 osync_plugin_config_is_supported
@@ -520,6 +521,7 @@ osync_plugin_config_remove_advancedoption
 osync_plugin_config_remove_resource
 osync_plugin_config_set_authentication
 osync_plugin_config_set_connection
+osync_plugin_config_set_externalplugin
 osync_plugin_config_set_localization
 osync_plugin_config_set_supported
 osync_plugin_config_unref
@@ -566,6 +568,11 @@ osync_plugin_env_new
 osync_plugin_env_ref
 osync_plugin_env_register_plugin
 osync_plugin_env_unref
+osync_plugin_externalplugin_get_external_command
+osync_plugin_externalplugin_new
+osync_plugin_externalplugin_ref
+osync_plugin_externalplugin_set_external_command
+osync_plugin_externalplugin_unref
 osync_plugin_finalize
 osync_plugin_get_config_type
 osync_plugin_get_data

opensync/CMakeLists.txt
@@ -60,6 +60,7 @@ SET( libopensync_LIB_SRCS
    plugin/opensync_plugin_info.c
    plugin/opensync_plugin_localization.c
    plugin/opensync_plugin_resource.c
+   plugin/opensync_plugin_externalplugin.c
    plugin/opensync_objtype_sink.c
    version/opensync_version.c
    xmlformat/opensync_xmlfield.c

opensync/client/opensync_client_proxy.c
@@ -952,7 +952,7 @@ OSyncMember *osync_client_proxy_get_member(OSyncClientProxy *proxy)
 	return proxy->member;
 }
 
-osync_bool osync_client_proxy_spawn(OSyncClientProxy *proxy, OSyncStartType type, const char *path, OSyncError **error)
+osync_bool osync_client_proxy_spawn(OSyncClientProxy *proxy, OSyncStartType type, const char *path, const char* external_command, OSyncError **error)
 {
 	OSyncQueue *read1 = NULL;
 	OSyncQueue *read2 = NULL;
@@ -1080,6 +1080,22 @@ osync_bool osync_client_proxy_spawn(OSyncClientProxy *proxy, OSyncStartType type
 			goto error;
 	} else {
 		name = osync_strdup_printf("%s%cpluginpipe", path, G_DIR_SEPARATOR);
+	
+		if (external_command) {
+			char *command = osync_strdup_printf(external_command, name);
+			osync_trace(TRACE_INTERNAL, "g_spawn_command_line_async(%s)", command);
+			GError *gerror = NULL;
+			gboolean f = g_spawn_command_line_async(command, &gerror);
+			if (!f) {
+				osync_error_set(error, OSYNC_ERROR_GENERIC, "Unable to g_spawn_command_line_async(%s): %s", command, gerror->message);
+				g_error_free (gerror);
+				osync_free(command);
+				goto error;
+			}
+			osync_free(command);
+		}
+
+
 		proxy->outgoing = osync_queue_new(name, error);
 		osync_free(name);
 		if (!proxy->outgoing)

opensync/client/opensync_client_proxy_internals.h
@@ -44,7 +44,7 @@ void osync_client_proxy_set_context(OSyncClientProxy *proxy, GMainContext *ctx);
 void osync_client_proxy_set_change_callback(OSyncClientProxy *proxy, change_cb cb, void *userdata);
 OSyncMember *osync_client_proxy_get_member(OSyncClientProxy *proxy);
 
-OSYNC_TEST_EXPORT osync_bool osync_client_proxy_spawn(OSyncClientProxy *proxy, OSyncStartType type, const char *path, OSyncError **error);
+OSYNC_TEST_EXPORT osync_bool osync_client_proxy_spawn(OSyncClientProxy *proxy, OSyncStartType type, const char *path, const char* external_command, OSyncError **error);
 OSYNC_TEST_EXPORT osync_bool osync_client_proxy_shutdown(OSyncClientProxy *proxy, OSyncError **error);
 
 OSYNC_TEST_EXPORT osync_bool osync_client_proxy_initialize(OSyncClientProxy *proxy, initialize_cb callback, void *userdata, const char *formatdir, const char *plugindir, const char *plugin, const char *groupname, const char *configdir, OSyncPluginConfig *config, OSyncError **error);

opensync/engine/opensync_engine.c
@@ -773,7 +773,11 @@ static OSyncClientProxy *_osync_engine_initialize_member(OSyncEngine *engine, OS
 	osync_client_proxy_set_context(proxy, engine->context);
 	osync_client_proxy_set_change_callback(proxy, _osync_engine_receive_change, engine);
 
-	if (!osync_client_proxy_spawn(proxy, osync_plugin_get_start_type(plugin), osync_member_get_configdir(member), error))
+	const char *external_command=NULL;
+	if (osync_plugin_get_start_type(plugin)==OSYNC_START_TYPE_EXTERNAL)
+		external_command=osync_member_get_external_command(member);
+
+	if (!osync_client_proxy_spawn(proxy, osync_plugin_get_start_type(plugin), osync_member_get_configdir(member), external_command, error))
 		goto error_free_proxy;
 	
 	engine->busy = TRUE;

opensync/group/opensync_member.h
@@ -334,6 +334,7 @@ OSYNC_EXPORT osync_bool osync_member_config_is_uptodate(OSyncMember *member);
  */
 OSYNC_EXPORT osync_bool osync_member_plugin_is_uptodate(OSyncMember *member);
 
+
 /*@}*/
 
 #endif /* _OPENSYNC_MEMBER_H_ */

commit dc7e7e998e17e5de10069be929b80dbe76c577a6

Fix various test-suite regression by commit r5896 which get caused by a segfault.
Due to a missing initization by NULL of an OSyncError *struct



git-svn-id: http://svn.opensync.org/trunk@5897 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

opensync/group/opensync_member.c
@@ -835,7 +835,7 @@ OSyncList *osync_member_get_objtypes(OSyncMember *member) {
 const char *osync_member_get_external_command(OSyncMember *member)
 {
 	osync_assert(member);
-	OSyncError *error;
+	OSyncError *error = NULL;
 	OSyncPluginConfig *config = osync_member_get_config_or_default(member, &error);
 	if (config) {
 		OSyncPluginExternalPlugin *externalplugin = osync_plugin_config_get_externalplugin(config);