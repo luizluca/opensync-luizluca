a3b509af846bc6a5b6161e511d50947d3aebdd0c
Daniel Gollub <gollub@b1-systems.de>
2009-11-05 20:12:29 +0000

    Reenable pending limit. This got temporarly disabled by r5858 to address issue described in #1161
    Finally we can commit/write more then 10 changes with the SyncML plugin,
    without loosing pending limit capability!
    
    
    
    git-svn-id: http://svn.opensync.org/trunk@5942 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

--- opensync/client/opensync_client.c
+++ opensync/client/opensync_client.c
@@ -1813,11 +1813,7 @@ osync_bool osync_client_set_incoming_queue(OSyncClient *client, OSyncQueue *inco
 
 	client->incoming = osync_queue_ref(incoming);
 
-	/* FIXME: temporarily disable pending limit.
-	 Re-enable this once the syncml pending limit deadlock has been fixed */
-#if 0
 	osync_queue_set_pending_limit(incoming, OSYNC_QUEUE_PENDING_LIMIT);
-#endif
 
 	return TRUE;
 
